0fba86afa9a9f2f3f22f9f9781bec98b23c854d0
Théodore <theodore@ch-le.fr>
2026-01-17 21:33:00 +0100

    added gitignore

--- bin/tests.d
+++
@@ -1,7 +0,0 @@
-bin/tests: tests/network_tests/network_tests.cpp \
- tests/network_tests/network_tests.hpp \
- tests/network_tests/../../cpp_tests/src/tests.hpp \
- tests/network_tests/../../src/network_input_handler/network_input_handler.hpp
-tests/network_tests/network_tests.hpp:
-tests/network_tests/../../cpp_tests/src/tests.hpp:
-tests/network_tests/../../src/network_input_handler/network_input_handler.hpp:
--- obj/network_input_handler/network_input_handler.d
+++
@@ -1,4 +0,0 @@
-obj/network_input_handler/network_input_handler.o: \
- src/network_input_handler/network_input_handler.cpp \
- src/network_input_handler/network_input_handler.hpp
-src/network_input_handler/network_input_handler.hpp:
